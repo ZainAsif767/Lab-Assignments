dd0abe15d222880cc39c6fda6dee6dc45d9ae954
zainasif767 <zainclasher2002@gmail.com>
2023-02-05 22:39:37 +0500

    LAb

---
+++ Lab 1/OutputOfPrograms.py
@@ -0,0 +1,16 @@
+# x = 6
+# if (type(x) is int):
+#     print("true")
+# else:
+#     print("false")
+
+#  (i) Use dir and help to learn about the
+#  functions you can call on dictionaries
+#  and implement it.
+dic1 = {'Apple': 20, 'Banana': 40, 'Mango': 75}
+dic2 = {'Kiwi': 110, 'Orange': 90}
+
+print(dic1.items())
+print(dic1.values())
+print(dic2.get('Orange'))
+print(dic2.keys())
