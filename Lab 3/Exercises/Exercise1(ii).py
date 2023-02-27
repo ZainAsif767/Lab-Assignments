cc9d812ff5d21bbb983fefa1f926ba599308dbbe
zainasif767 <zainclasher2002@gmail.com>
2023-02-27 19:59:36 +0500

    Ex 2

---
+++ Lab 3/Exercises/Exercise1(ii).py
@@ -0,0 +1,11 @@
+# Python program to find if a given string starts
+# with a given character using Lambda
+
+def starts_with(x): return True if x.startswith('S') else False
+
+
+print(starts_with('Syed Zain Asif'))
+def starts_with(x): return True if x.startswith('A') else False
+
+
+print(starts_with('Amir'))
